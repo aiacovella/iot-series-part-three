737dce063d6db81b87acf9103cea3e98b986c36c
aiacovella <al.iacovella@gmail.com>
2016-08-05 12:58:57 -0400

    modified to read ir,uv and light readings

--- particle/moisturesensor.ino
+++ particle/moisturesensor.ino
@@ -1,5 +1,13 @@
 // This #include statement was automatically added by the Particle IDE.
 #include "MQTT/MQTT.h"
+#include <application.h>
+#include <spark_wiring_i2c.h>
+
+// SI1132 I2C address is 0x60(96)
+#define Addr 0x60
+
+double visible = 0.0, ir = 0.0, uv = 0.0;
+int response = 0;
 
 // Configure the incoming signal pin for the moisture sensor.
 int sensorPin = A0;
@@ -11,7 +19,14 @@ int powerOut = D0;
 int diagnosticLed = D7;
 
 // Unique identifier for the device
-String deviceId="/garden/aisle/1/moisture/1";
+String deviceId="/garden/aisle/1/";
+
+// Unique identifier for individual sensors
+String moistureSensor = deviceId + "moisture";
+String visibleLightSensor = deviceId + "visible-light";
+String infraRedSensor = deviceId + "infra-red";
+String ultraVioletSensor = deviceId + "ultra-violet";
+
 
 // Configure the IP address of the MQTT Client 
 byte server[] = { 192,168,117,86 };
@@ -38,6 +53,10 @@ void setup() {
 
   // Connect the the spark MQTT service.
   client.connect("sparkclient");
+  
+  Serial.println("Setting up light sensor");
+
+  setupLightSensor();
 
 }
 
@@ -54,7 +73,7 @@ void loop() {
   int sensorValue = analogRead(sensorPin);
 
   // Send the moisture reading for this device to the particle cloud service
-  bool result = Particle.publish("moisture", String(sensorValue));    
+  bool result = Particle.publish(moistureSensor, String(sensorValue));    
   Serial.println("Publish of Sensor value " + String(sensorValue) + " to the Particle Cloud returned " + String(result));
 
 
@@ -67,9 +86,13 @@ void loop() {
       client.connect("sparkclient");
   }
 
-// Send the moisture reading for this device to the MQTT service
-  bool mqttResult = client.publish(deviceId, String(sensorValue));
+  // Send the moisture reading for this device to the MQTT service
+  bool mqttResult = client.publish(moistureSensor, String(sensorValue));
   Serial.println("Publish of Sensor value " + String(sensorValue) + " to the MQTT server returned " + String(mqttResult));
+
+
+  // Publish the IR and UV readings
+  publishLightSensorReadings();
 
 
   // Turn the power on the sensor back off to prevent the sensor from degrading
@@ -77,7 +100,14 @@ void loop() {
   digitalWrite(powerOut, LOW);    
 
   // Pause for 60 seconds until the next reading
-  delay(60000);
+  Serial.println("Sleeping");
+  System.sleep(SLEEP_MODE_DEEP, 180);
+  //System.sleep(240000);
+  
+  //delay(60000);
+  
+  Serial.println("Awake");
+
 
 }
 
@@ -85,23 +115,417 @@ void loop() {
 void blinkDiagnostic(int numberOfBlinks, int duration){
   int cnt = 1;
 
-  while(cnt &lt;= numberOfBlinks) {
-    // We&#039;ll turn the LED on
+  while(cnt <= numberOfBlinks) {
+    // We'll turn the LED on
     digitalWrite(diagnosticLed, HIGH);
   
-    Serial.println(&quot;=== Turn On&quot;);
+    Serial.println("=== Turn On");
 
     delay(duration);
     
-    // Then we&#039;ll turn it off...
+    // Then we'll turn it off...
     digitalWrite(diagnosticLed, LOW);
 
-    Serial.println(&quot;=== Turn Off&quot;);
+    Serial.println("=== Turn Off");
 
     delay(duration);
 
     cnt += 1;
     
-    Serial.println(&quot;=== Count &quot; + String(cnt));
+    Serial.println("=== Count " + String(cnt));
   }
+
 }
+
+void setupLightSensor() {
+  Serial.println("Setting up light sensor");    
+
+// Particle.variable("i2cdevice", "SI1132");
+//   Particle.variable("visible", visible);
+//   Particle.variable("uv", uv);
+//   Particle.variable("ir", ir);
+  
+  // Initialise I2C communication as MASTER
+  Wire.begin();
+  // Initialise Serial Communication, Baud rate = 9600
+//  Serial.begin(9600);
+
+  // Enable UVindex measurement coefficients
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select COFF-1 register
+  Wire.write(0x13);
+  // Default value
+  Wire.write(0x29);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select COFF-2 register
+  Wire.write(0x14);
+  // Default value
+  Wire.write(0x89);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select COFF-3 register
+  Wire.write(0x15);
+  // Default value
+  Wire.write(0x02);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+
+
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select COFF-4 register
+  Wire.write(0x16);
+  // Default value
+  Wire.write(0x00);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select PARAM_WR register
+  Wire.write(0x17);
+  // Enable uv, Visible, IR
+  Wire.write(0xF0);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select command register
+  Wire.write(0x18);
+  // Select CHLIST register in RAM
+  Wire.write(0x01 | 0xA0);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+  delay(10);
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select PARAM_RD register
+  Wire.write(0x2E);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+
+  // Request 1 byte of data
+  Wire.requestFrom(Addr, 1);
+  // Read 1 byte of data
+  response = Wire.read();
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select HW_KEY register
+  Wire.write(0x07);
+  // Default value
+  Wire.write(0x17);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select PARAM_WR register
+  Wire.write(0x17);
+  // Small IR photodiode
+  Wire.write(0x00);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select COMMAND register
+  Wire.write(0x18);
+  // Select ALS_IR_ADCMUX register in RAM
+  Wire.write(0x0E | 0xA0);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+  delay(10);
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select PARAM_RD register
+  Wire.write(0x2E);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+
+  // Request 1 byte of data
+  Wire.requestFrom(Addr, 1);
+  // Read 1 byte of data
+  response = Wire.read();
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select PARAM_WR register
+  Wire.write(0x17);
+  // Set ADC Clock divided / 1
+  Wire.write(0x00);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select COMMAND register
+  Wire.write(0x18);
+  // Select ALS_IR_ADC_GAIN register in RAM
+  Wire.write(0x1E | 0xA0);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+  delay(10);
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select PARAM_RD register
+  Wire.write(0x2E);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+
+  // Request 1 byte of data
+  Wire.requestFrom(Addr, 1);
+  // Read 1 byte of data
+  response = Wire.read();
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select PARAM_WR register
+  Wire.write(0x17);
+  // Set 511 ADC Clock
+  Wire.write(0x70);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select COMMAND register
+  Wire.write(0x18);
+  /// Select ALS_IR_ADC_COUNTER register in RAM
+  Wire.write(0x1D | 0xA0);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+  delay(10);
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select PARAM_RD register
+  Wire.write(0x2E);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+
+  // Request 1 byte of data
+  Wire.requestFrom(Addr, 1);
+  // Read 1 byte of data
+  response = Wire.read();
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select PARAM_WR register
+  Wire.write(0x17);
+  // Set ADC Clock divided / 1
+  Wire.write(0x00);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select COMMAND register
+  Wire.write(0x18);
+ // Select ALS_VIS_ADC_GAIN register in RAM
+  Wire.write(0x11 | 0xA0);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+  delay(10);
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select PARAM_RD register
+  Wire.write(0x2E);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+
+  // Request 1 byte of data
+  Wire.requestFrom(Addr, 1);
+  // Read 1 byte of data
+  response = Wire.read();
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select PARAM_WR register
+  Wire.write(0x17);
+  // High Signal Range
+  Wire.write(0x20);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select COMMAND register
+  Wire.write(0x18);
+  // Select ALS_IR_ADC_MISC register in RAM
+  Wire.write(0x1F | 0xA0);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+  delay(10);
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select PARAM_RD register
+  Wire.write(0x2E);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+
+  // Request 1 byte of data
+  Wire.requestFrom(Addr, 1);
+  // Read 1 byte of data
+  response = Wire.read();
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select PARAM_WR register
+  Wire.write(0x17);
+  // Set 511 ADC Clock
+  Wire.write(0x70);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select COMMAND register
+  Wire.write(0x18);
+  // Select ALS_VIS_ADC_COUNTER register in RAM
+  Wire.write(0x10 | 0xA0);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+  delay(10);
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select PARAM_RD register
+  Wire.write(0x2E);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+
+  // Request 1 byte of data
+  Wire.requestFrom(Addr, 1);
+  // Read 1 byte of data
+  response = Wire.read();
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select PARAM_WR register
+  Wire.write(0x17);
+  // High Signal Range
+  Wire.write(0x20);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select COMMAND register
+  Wire.write(0x18);
+  // Select ALS_VIS_ADC_MISC register in RAM
+  Wire.write(0x12 | 0xA0);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+  delay(10);
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select PARAM_RD register
+  Wire.write(0x2E);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+
+  // Request 1 byte of data
+  Wire.requestFrom(Addr, 1);
+  // Read 1 byte of data
+  response = Wire.read();
+  delay(300);
+
+}
+
+
+void publishLightSensorReadings() {
+  unsigned int data[4];
+  
+  Serial.println("Publishing Light Sensor Readings");
+  
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select COMMAND register
+  Wire.write(0x18);
+  // Start ALS conversion
+  Wire.write(0x0E);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+  delay(500);
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select data register
+  Wire.write(0x22);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+  
+  // Request 4 byte of data
+  Wire.requestFrom(Addr, 4);
+
+  // Read 4 bytes of data
+  // visible lsb, visible msb, ir lsb, ir msb
+  if (Wire.available() == 4)
+  {
+    data[0] = Wire.read();
+    data[1] = Wire.read();
+    data[2] = Wire.read();
+    data[3] = Wire.read();
+  }
+
+  visible = (data[1] * 256.0 + data[0]);
+  ir = (data[3] * 256.0 + data[2]);
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select data register
+  Wire.write(0x2C);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+  
+  // Request 2 bytes of data
+  Wire.requestFrom(Addr, 2);
+
+  // Read 2 bytes of data
+  // uv lsb, uv msb
+  if (Wire.available() == 2)
+  {
+    data[0] = Wire.read();
+    data[1] = Wire.read();
+  }
+  
+  // Convert the data
+  uv = (data[1] * 256.0 + data[0]);
+
+  //Pulbish the visible light values to the cloud and MQTT
+  Particle.publish(visibleLightSensor, String(visible));
+  client.publish(visibleLightSensor, String(visible));
+  delay(1000);
+
+  //Pulbish the infra-red values to the cloud and MQTT
+  Particle.publish(infraRedSensor, String(ir));
+  client.publish(infraRedSensor, String(ir));
+  delay(1000);
+
+  Particle.publish(ultraVioletSensor, String(uv));
+  client.publish(ultraVioletSensor, String(uv));
+
+  delay(1000);
+}
+
+
+
